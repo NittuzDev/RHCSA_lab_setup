173f4b2a10fe3fe3337e7d76bf3f9b2a276c5656
nicola <nicola_freccia92@hotmail.it>
2026-02-10 18:19:08 +0100

    newfile

---
+++ setup_AutoFS_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum install nfs* -y
+systemctl enable nfs-utils
+systemctl enable nfs-server
+mkdir -p /home/guests
+mkdir -p /home/IT_Database{1..20}
+cd /home/guests/
+chown -R nobody:nobody /home/guests
+chcon -R -t nfs_t /home/guests/
+echo "/home/guests  *(rw,sync)" >> /etc/exports
+firewall-cmd --permanent --add-service={nfs,mountd,rpc-bind}
+firewall-cmd --reload
+systemctl restart nfs-utils
+systemctl restart nfs-server
